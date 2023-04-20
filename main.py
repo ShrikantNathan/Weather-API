4dd142eaf79b529626af3f4836837ca867576a2e
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-04-20 09:32:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,67 @@
 import os
 import psycopg2
-import numpy as np
-from typing import List, Union, AnyStr
 
-class WeatherDataAPI:
-    def __init__(self):
-        self.connector = psycopg2.connect(database="postgres", user="postgres", password="Lotus@123", host="localhost",
-                                     port=5432)
-        self.cursor = self.connector.cursor()
-        print('Connected to Server ->', not self.cursor.closed)
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="testing123"
+)
 
-    def connect_server_and_ingest_data(self):
-        try:
-            for wx_file in os.listdir(os.path.join(os.getcwd(), 'code-challenge-template', 'wx_data')):
-                with open(os.path.join(os.getcwd(), "code-challenge-template", "wx_data", wx_file), 'r') as file:
-                    # cursor.copy_from(file, 'tbl_weather', sep=',')
-                    for line in file.readlines():
-                        filtered = line.strip().split()
-                        self.cursor.execute("INSERT INTO tbl_weather VALUES (TO_DATE(%s, 'YYYYMMDD'), %s, %s, %s)", (filtered[0], filtered[1], filtered[2], filtered[3]))
-                        # print(filtered[0])
-                self.connector.commit()
-            print('Data inserted successfully')
+# Create a cursor object
+cur = conn.cursor()
 
-        except (Exception, psycopg2.Error) as e:
-            print('Error while inserting data into the server', e)
+# Loop through each file in the wx_data directory
+for filename in os.listdir(os.path.join(os.getcwd(), "code-challenge-template", "wx_data")):
+    if filename.endswith(".txt"):
+        with open(os.path.join(os.getcwd(), "code-challenge-template", "wx_data", filename), "r") as f:
+            # Get the station name from the filename
+            station = filename.split(".")[0]
 
-        finally:
-            if self.connector:
-                self.cursor.close()
-                self.connector.close()
-                print('Disconnected', self.connector.closed)
+            # Initialize dictionaries to store data for each year
+            max_temps = {}
+            min_temps = {}
+            precipitations = {}
 
-    def fetch_all_weather_records(self):
-        self.cursor.execute("SELECT * FROM tbl_weather")
-        record = self.cursor.fetchall()
-        for rec in record:
-            print(rec[0], rec[1], rec[2], rec[3])
+            # Loop through each line in the file
+            for line in f:
+                # Split the line into its components
+                date, max_temp, min_temp, precipitation = line.strip().split("\t")
 
-    def fetch_distinct_weather_records_and_calculate(self):
-        """ This action will loop over the table, fetch distinct rows, and perform every calculations on each
-        rows and insert it to the database"""
-        import operator
-        self.cursor.execute("SELECT DISTINCT w_date, max_temp, min_temp, amt_precipitat FROM tbl_weather;")
-        record = self.cursor.fetchall()
-        for rec in record:
-            w_date, max_temp, min_temp, amt_precip = rec[0], rec[1], rec[2], rec[3]
-            temp_fahrenheit = operator.add(operator.mul(1.8, max_temp), 32)
-            temp_celsius = operator.truediv(operator.sub(temp_fahrenheit, 32), 1.8)
-            mean_max_temp, mean_min_temp = np.mean(rec[1]), np.mean(rec[2]) # Mean Max and Min Avg Temp calculated
-            acc_precipit = 
+                # Ignore missing data
+                if max_temp == "-9999" or min_temp == "-9999" or precipitation == "-9999":
+                    continue
 
+                # Extract the year from the date
+                year = int(date[:4])
 
-# WeatherDataAPI().connect_server_and_ingest_data()
-WeatherDataAPI().fetch_distinct_weather_records_and_calculate()
+                # Calculate statistics for this year
+                if year not in max_temps:
+                    max_temps[year] = []
+                if year not in min_temps:
+                    min_temps[year] = []
+                if year not in precipitations:
+                    precipitations[year] = []
 
+                max_temps[year].append(float(max_temp) / 10)
+                min_temps[year].append(float(min_temp) / 10)
+                precipitations[year].append(float(precipitation) / 10)
+
+            # Calculate statistics for each year and insert them into the database
+            for year in max_temps:
+                avg_max_temp = sum(max_temps[year]) / len(max_temps[year])
+                avg_min_temp = sum(min_temps[year]) / len(min_temps[year])
+                total_precipitation = sum(precipitations[year]) / 10
+
+                cur.execute(
+                    "INSERT INTO weather_statistics (year, station, avg_max_temperature, avg_min_temperature, total_precipitation) VALUES (%s, %s, %s, %s, %s)",
+                    (year, station, avg_max_temp, avg_min_temp, total_precipitation))
+            print("Data Inserted Successfully.")
+            print(f'{cur.rowcount} row(s) inserted')
+        # Commit changes to the database after processing each file
+        conn.commit()
+
+# Close the cursor and connection objects
+cur.close()
+conn.close()
